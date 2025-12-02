e353b681e1bd5827f7f80bc77a2353b2d655daa9
Piyush Bhawsar <pbhawsar2001@gmail.com>
2025-12-02 13:30:37 +0530

    Update Mail.js

--- backend/configs/Mail.js
+++ backend/configs/Mail.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 dotenv.config()
 const transporter = nodemailer.createTransport({
   service: "Gmail",
-  secure: true, 
+  secure: false, 
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,
